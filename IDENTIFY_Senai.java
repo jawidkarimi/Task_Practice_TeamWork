1c38b95d5eec3aff3b906f0050ccbdb59447076a
Senai <simoseni@gmail.com>
2023-05-25 07:49:42 -0700

    IDENTIFY Task Created.

---
+++ IDENTIFY_Senai.java
@@ -0,0 +1,19 @@
+public class IDENTIFY_Senai {
+    public static void main(String[] args) {
+        identify(8);
+        identify(7);
+
+
+    }
+    public static void  identify(int number) {
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else if(number%2!=0) {
+            System.out.println("Odd");
+        }else{
+            System.out.println("Zero");
+        }
+
+    }
+
+}
