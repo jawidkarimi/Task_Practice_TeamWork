ba716a8a871308ade313d8e74a12b6e68313a36e
Ono <ogerel@gmail.com>
2023-05-24 19:34:02 -0700

    Added Task 1.

---
+++ OddOrEven.java
@@ -0,0 +1,14 @@
+public class OddOrEven{
+    public static void main(String[] args) {
+
+        OddOrEven(100);
+    }
+
+    public static void OddOrEven(int number){
+        if (number % 2 == 0) {
+            System.out.println(number + " is even number");
+        } else {
+            System.out.println(number + " is odd number");
+        }
+    }
+}
