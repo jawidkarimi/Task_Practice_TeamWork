a6f64b706748f9a57ce807baca3f59276f08c048
Senai <simoseni@gmail.com>
2023-05-25 07:45:25 -0700

    FINRA task created

---
+++ FINRA_Senai.java
@@ -0,0 +1,23 @@
+public class FINRA_Senai {
+    public static void printFINRA(int num){
+
+        for ( int i = 1; i <= num; i++) {    // 1 ---30
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FINRA");
+            } else if (i % 3 == 0) {
+                System.out.println("FIN");
+            } else if (i % 5 == 0) {
+                System.out.println("RA");
+            } else {
+                System.out.println(i);
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        printFINRA(5);
+    }
+
+}
