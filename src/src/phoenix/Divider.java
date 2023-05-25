e95c74a36a3756ec3da78ab3642b86b4d76b0e7b
Musti_Cydeo <elbozan@hotmail.com>
2023-05-25 03:16:55 -0600

    Divider added FinRa added OddOrEven added

---
+++ src/src/phoenix/Divider.java
@@ -0,0 +1,32 @@
+package phoenix;
+
+import java.util.Scanner;
+
+public class Divider {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter the number a and number b to see the result a / b");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int result = Divider(a, b);
+
+        System.out.println("Result: " + result);
+
+    }
+
+    public static int Divider(int a, int b){
+      int c = 0;
+
+      while( a >= b){
+          a -= b;
+          c++;
+      }
+        return c;
+    }
+
+
+    }
+
+
