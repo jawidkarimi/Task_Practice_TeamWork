124dfa045c76c6c0b82f5596235f513d9ff0edf6
khali <khalil.elkhoja@hotmail.co.uk>
2023-05-25 17:40:19 -0700

    Added the divide task

---
+++ Khalil/khalil_DivideWithoutOperator.java
@@ -0,0 +1,19 @@
+package Khalil;
+
+public class khalil_DivideWithoutOperator {
+
+    public static void numbers(int num1,int num2) {
+
+        int result = 0;
+
+        while(num1>=num2){
+            num1=num1-num2;
+            result++;
+        }
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        numbers(9,8);
+    }
+}
