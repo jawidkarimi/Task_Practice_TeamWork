6c3dc4c056c7561f0d729a9f427cee70e5aca804
Musti_Cydeo <elbozan@hotmail.com>
2023-06-03 20:33:23 -0600

    projects updated for weeks separately for the first 3 weeks

---
+++ src/milgin46/week2/SwapNumbers.java
@@ -0,0 +1,30 @@
+package src.milgin46.week2;
+
+import java.util.Scanner;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        int a = scan.nextInt();
+        System.out.println("Enter b: ");
+        int b = scan.nextInt();
+        System.out.println("Before swap a is:" + a + " - b is: " + b);
+
+        a = a + b;
+        b = a - b; //  a + b - b
+        a = a - b; // a + b - a
+
+
+        System.out.println(a);
+        System.out.println(b);
+
+        System.out.println("After swap a is: " + a + " - b is: " + b);
+
+    }
+}
+/*
+1️⃣ Numbers -- Swap Numbers
+Swap two variable values without using a third variable
+ */
---
+++ src/milgin46/week3/PrimeNumber.java
@@ -0,0 +1,48 @@
+package src.milgin46.week3;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        System.out.println(primenumber(19));
+
+        System.out.println(primenumber(25));
+
+        System.out.println(primenumber(125));
+
+        System.out.println(primenumber(197));
+
+        System.out.println(primenumber(1234));
+
+        System.out.println(primenumber(97));
+
+    }
+
+    public static String primenumber(int n) {
+        String pr = "";
+
+
+
+            if (n <= 1) {
+                return "not a prime";
+            } // 0 & 1 are not prime numbers
+
+            for (int i = 2; i < n ; i++) { // if any number doesnt include a number from 2 until the number itself minus 1 then it is prime
+                if (n % i == 0) {
+                    return "not a prime";
+                }
+            }
+
+            return "a prime";
+        }
+
+
+    }
+
+/*
+
+1️⃣ Numbers -- PrimeNumber
+Write a method that can check if a number is prime or not
+
+
+
+ */
