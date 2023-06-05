250aba9b7d2f5867eed9345960d690d27bf28557
Ono <ogerel@gmail.com>
2023-06-04 20:51:17 -0700

    Updating to Ono folder

--- Ono/DivideTwoNumbers.java
+++ Ono/DivideTwoNumbers.java
@@ -1,3 +1,5 @@
+package Ono;
+
 public class DivideTwoNumbers {
 
     public static void main(String[] args) {
--- Ono/OddOrEven.java
+++ Ono/OddOrEven.java
@@ -1,3 +1,5 @@
+package Ono;
+
 public class OddOrEven{
     public static void main(String[] args) {
 
