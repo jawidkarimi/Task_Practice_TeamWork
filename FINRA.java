da8e2dcedaac36efb7939298fb0d2b7d82a71bdd
MFadli603 <elfadili.med4@gmail.com>
2023-06-08 09:40:51 -0700

    Finra uPdate

--- FINRA.java
+++ FINRA.java
@@ -4,19 +4,24 @@ public class FINRA {
         finra(30);
 
     }
-    public static void finra(int n){
-        String result ="";
 
-        for (int i = 1; i <=n; i++) {
-            if(i%15 == 0){
-                result+="FINRA ";
-            } else if(i%5==0){
-                result+="RA ";
-            } else if(i %3 ==0){
-                result+="FIN ";
-            } else{
-                result += i+" ";
+    public static void finra(int n) {
+        String result = "";
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                result += "FINRA ";
+            } else if (i % 5 == 0) {
+                result += "RA ";
+            } else if (i % 3 == 0) {
+                result += "FIN ";
+            } else {
+                result += i + " ";
+            }
+        }
+    }
 }
+
 /*
 Write a method which prints out the numbers from 1 to 30 but for numbers which
 are a multiple of 3 print "FIN" instead of the number and for numbers which are a
