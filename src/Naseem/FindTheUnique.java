a2e9b2367297b06ce510e8f1d53bdaba4b176127
naseemalshareef <alshareefnaseem86@gmail.com>
2023-06-21 23:17:36 -0700

    Added Task2  FinddUnique litter updated1....

---
+++ src/Naseem/FindTheUnique.java
@@ -0,0 +1,32 @@
+package src.Naseem;
+
+public class FindTheUnique {
+
+    public static String UniqueCharacters(String str) {
+
+
+        String unique = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char each = str.charAt(i);
+
+            if (str.indexOf(each) == str.lastIndexOf(each)) {
+                unique += each;
+            }
+
+        }
+        return unique;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(UniqueCharacters("AAABBBCCCDEF"));
+    }
+
+}
+
+
+
+
+
+
+
