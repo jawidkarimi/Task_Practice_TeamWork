ea1437d50fc1f55a8264113984ab03d3c7cde2a0
khali <khalil.elkhoja@hotmail.co.uk>
2023-06-26 15:05:46 -0700

    Added Java task (week 6)!!

--- src/khalil/Khalil_StringPasswordValidation.java
+++ src/khalil/Khalil_StringPasswordValidation.java
@@ -15,10 +15,7 @@ public class Khalil_StringPasswordValidation {
     }
 
     public static boolean isValid(String password) {
-        //return true if and only if password:
-        //1. have at least eight characters.
-        //2. consists of only letters and digits.
-        //3. must contain at least two digits.
+
         /*
         1. Password MUST be at least have 6 characters and should not contain space
         2. PassWord should at least contain one upper-case letter
@@ -26,22 +23,8 @@ public class Khalil_StringPasswordValidation {
         4. Password should at least contain one special character
         5. Password should at least contain a digit
         */
-        if (password.length() <= 6) {
+        if (password.length() <= 6 || password.contains(" ")) {
             return false;
-        } else {
-            char c;
-            int count = 1;
-            for (int i = 0; i < password.length() - 1; i++) {
-                c = password.charAt(i);
-                if (!Character.isLetterOrDigit(c)) {
-                    return false;
-                } else if (Character.isDigit(c)) {
-                    count++;
-                    if (count < 2)   {
-                        return false;
-                    }
-                }
-            }
         }
         return true;
     }
