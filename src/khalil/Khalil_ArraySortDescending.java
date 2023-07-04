36f4e4a132088b4c7ac3d9be7804ba6b4784bbab
khali <khalil.elkhoja@hotmail.co.uk>
2023-07-04 13:47:47 -0700

    Added Java task (week 7)!!

--- src/khalil/Khalil_ArraySortDescending.java
+++ src/khalil/Khalil_ArraySortDescending.java
@@ -2,16 +2,16 @@ package src.khalil;
 
 import java.util.Arrays;
 
-public class Khalil_ArraySortAscending {
+public class Khalil_ArraySortDescending {
 
     public static void main(String[] args) {
 
-        int a[]={9,8,0,5};
+        int a[]={9,8,0,3};
         System.out.println(Arrays.toString(a));
         int temp;
         for(int i=0;i<a.length-1;i++){
             for(int j=0;j<a.length-1;j++){
-                if(a[j] > a[j+1]){
+                if(a[j] < a[j+1]){
                     temp = a[j+1];
                     a[j+1] = a[j];
                     a[j]=temp;
@@ -21,6 +21,5 @@ public class Khalil_ArraySortAscending {
         System.out.println(Arrays.toString(a));
 
     }
-
 
 }
